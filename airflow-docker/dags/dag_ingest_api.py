7e831ccccd2d1359346aac7afdc4ab0382713f3e
Rodrigo Sales Marcolin <rodrigosalesmarcolin@gmail.com>
2023-05-02 20:46:03 -0300

    new dag layout

---
+++ airflow-docker/dags/dag_ingest_api.py
@@ -0,0 +1,53 @@
+from airflow.decorators import task, dag
+from airflow.models import Variable
+from datetime import timezone
+from airflow import DAG
+from datetime import datetime
+from src.ETL.extract_data import get_route 
+import pandas as pd
+
+with DAG(
+    "extract_file", # Nome da dag
+    schedule="30 11 * * *",
+    start_date=datetime(2022, 12, 4, 10, 0),
+    catchup=False,
+    max_active_tasks=10,
+) as dag:
+    """
+    DAG que ingere dados da API do TMDB, carregando-os no BigQuery
+
+    Define uma query com filtros e paraleliza a ingestão das páginas
+    """
+    @task
+    def extract(doc):
+        params = {
+            'primary_release_date.gte': '2023-01-01',
+            'primary_release_date.lte': '2023-06-01',
+        }
+
+        x = get_route(
+            route='/discover/:t', 
+            media_type='movie', 
+            details=[], 
+            initial_page = 1, 
+            final_page = 12, 
+            params=params
+        )
+
+        return doc
+
+    @task
+    def transform(doc):
+        return doc
+    
+    @task
+    def load(doc):
+        return doc
+
+    load(transform(extract({})))
+
+
+if __name__ == "__main__":
+    dag.test(
+        execution_date=datetime(2023, 4, 9, 12, 0, 0, tzinfo=timezone.utc)
+    )
